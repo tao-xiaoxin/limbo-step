6caba4866c6acac04cdadfb54ae3ccd7e6725744
xunichanghuan <zhpjake@vip.qq.com>
2021-08-12 03:23:48 +0000

    避开UTC 0点高峰期

--- random_cron.sh
+++ random_cron.sh
@@ -2,9 +2,9 @@
 
 sed -i '/cron:/d' .github/workflows/run.yml
 
-if [[ $(date '+%H') == 00 ]];then
-    time=$(($RANDOM%10))" 2 * * *"
-elif [[ $(date '+%H') == 02 ]];then
+if [[ $(date '+%H') == 01 ]];then
+    time=$(($RANDOM%10))" 3 * * *"
+elif [[ $(date '+%H') == 03 ]];then
     time=$(($RANDOM%10))" 5 * * *"
 elif [[ $(date '+%H') == 05 ]];then
     time=$(($RANDOM%10))" 7 * * *"
@@ -15,9 +15,9 @@ elif [[ $(date '+%H') == 09 ]];then
 elif [[ $(date '+%H') == 11 ]];then
     time=$(($RANDOM%5))" 13 * * *"
 elif [[ $(date '+%H') == 13 ]];then
-    time=$(($RANDOM%10))" 0 * * *"
+    time=$(($RANDOM%10))" 1 * * *"
 else
-    time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
+    time=$(($RANDOM%10))" 1,3,5,7,9,11,13 * * *"
 fi
 echo 当前时间为$(date '+%H')时，计划运行时间$time
 sed -i '/schedule/a\    - cron: '$time'' .github/workflows/run.yml
