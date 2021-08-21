f1c17bceb4764e04543eb9a9d1588fd7dda90a17
xunichanghuan <zhpjake@vip.qq.com>
2021-08-21 21:16:46 +0800

    Update random_cron.sh

--- random_cron.sh
+++ random_cron.sh
@@ -19,8 +19,11 @@ sed -i '/cron:/d' .github/workflows/run.yml
 # else
 #     time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
 # fi
-time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
-echo 当前时间为$(date '+%H')时，计划运行时间$time
-sed -i '/schedule/a\    - cron: '$time'' .github/workflows/run.yml
-
-exit 0
+if [[ $(date '+%H') == 00 or $(date '+%H') == 02 or $(date '+%H') == 05 or $(date '+%H') == 07 or $(date '+%H') == 09 or $(date '+%H') == 11 or $(date '+%H') == 13 ]];then
+    time=$(($RANDOM%10))" 0,2,5,7,9,11,13 * * *"
+    echo 当前时间为$(date '+%H')时，计划运行时间$time
+    sed -i '/schedule/a\    - cron: '$time'' .github/workflows/run.yml
+    exit 0
+else
+    exit 0
+fi
