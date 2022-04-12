c50d5e79218f96259485a7ff43ae4ad04ff31b2a
sunshine <53550304+resonates@users.noreply.github.com>
2022-04-12 16:55:27 +0800

    重写sqlalchemy的query,增加序列化方法

--- applications/extensions/init_sqlalchemy.py
+++ applications/extensions/init_sqlalchemy.py
@@ -55,15 +55,33 @@ class Query(BaseQuery):
     def logic_all(self):
         return self.filter_by(delete_at=None).all()
 
+    def all_json(self, schema: Marshmallow().Schema):
+        return schema(many=True).dump(self.all())
+
     def layui_paginate(self):
-        """
-        layui表格分页
-        page
-        limit
-        """
         return self.paginate(page=request.args.get('page', type=int),
                              per_page=request.args.get('limit', type=int),
                              error_out=False)
+
+    def layui_paginate_json(self, schema: Marshmallow().Schema):
+        """
+        返回dict
+        """
+        _res = self.paginate(
+            page=request.args.get('page', type=int),
+            per_page=request.args.get('limit', type=int),
+            error_out=False
+        )
+        return schema(many=True).dump(_res.items), _res.total, _res.page, _res.per_page
+
+    def layui_paginate_db_json(self):
+        """
+        db.query(A.name).layui_paginate_db_json()
+        """
+        _res = self.paginate(page=request.args.get('page', type=int),
+                             per_page=request.args.get('limit', type=int),
+                             error_out=False)
+        return [dict(i) for i in _res.items], _res.total
 
 
 db = SQLAlchemy(query_class=Query)
