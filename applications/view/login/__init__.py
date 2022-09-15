fce64d7cde1eee2083c1abc5b3505d7bce6c4e97
taoxiaoxin <202145875@qq.com>
2022-09-15 11:04:33 +0800

    deleted login_view

--- applications/view/login/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-from applications.view.login.index import login_index
-
-
-def register_login_views(app: Flask):
-    app.register_blueprint(login_index)
--- applications/view/login/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint
-
-login_index = Blueprint('login_index', __name__, url_prefix='/login/index')
-
-
-@login_index.route('/')
-def index():
-    return "这是login/index路由"
